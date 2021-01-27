ccb69cca3905b211393fb9bc1088944d5cb3a456
Jayson Rice <jayson.rice.95@gmail.com>
2021-01-26 20:20:36 -0500

    Updates

---
+++ precache-manifest.4c911ffc9c2038b4df4e496b0a20074f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c7dd93d9cf9ea0236ad82743d8406a",
+    "url": "/sketch-book/index.html"
+  },
+  {
+    "revision": "e38852365e62f5aa1b9d",
+    "url": "/sketch-book/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "5cdb8f70192c53aef0dc",
+    "url": "/sketch-book/static/js/2.8a35e28d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/sketch-book/static/js/2.8a35e28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9873c4e4935d4e2a1bfe",
+    "url": "/sketch-book/static/js/3.0b7c69e6.chunk.js"
+  },
+  {
+    "revision": "e38852365e62f5aa1b9d",
+    "url": "/sketch-book/static/js/main.570dd0a5.chunk.js"
+  },
+  {
+    "revision": "65ffc8af79b655a6af33",
+    "url": "/sketch-book/static/js/runtime-main.04cf0cc5.js"
+  }
+]);
